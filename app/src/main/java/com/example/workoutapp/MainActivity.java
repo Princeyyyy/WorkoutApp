24da8af662fc9fda2e30c65560a1f80a156f6122
Princeyyyy <princemumo80@gmail.com>
2020-11-13 22:50:15 +0300

    binded the various id using butterknife

--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.workoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.startWorkout) Button mStartWorkout;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
