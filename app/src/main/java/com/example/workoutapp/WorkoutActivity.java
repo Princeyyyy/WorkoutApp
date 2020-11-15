dd38e16704293431610a3c3ded4f0af85635eaa6
Princeyyyy <princemumo80@gmail.com>
2020-11-15 23:33:57 +0300

    binded id using butterknife

--- app/src/main/java/com/example/workoutapp/WorkoutActivity.java
+++ app/src/main/java/com/example/workoutapp/WorkoutActivity.java
@@ -3,8 +3,19 @@ package com.example.workoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class WorkoutActivity extends AppCompatActivity {
+    @BindView(R.id.continueWorkoutButton) Button mcontinueWorkoutButton;
+    @BindView(R.id.titlepage) TextView mtitlepage;
+    @BindView(R.id.subtitlepage) TextView msubtitlepage;
+    @BindView(R.id.intropage) TextView mintropage;
+    @BindView(R.id.subintropage) TextView msubintropage;
+    @BindView(R.id.divpage) View mdivpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
