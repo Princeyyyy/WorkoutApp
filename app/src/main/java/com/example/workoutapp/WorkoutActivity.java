ebe6be965a2e6cc9ea539f71edb97c6e24ed9c62
Princeyyyy <princemumo80@gmail.com>
2020-11-16 23:53:36 +0300

    updated the buttons

--- app/src/main/java/com/example/workoutapp/WorkoutActivity.java
+++ app/src/main/java/com/example/workoutapp/WorkoutActivity.java
@@ -11,15 +11,14 @@ import android.widget.TextView;
 import butterknife.BindView;
 
 public class WorkoutActivity extends AppCompatActivity {
-    @BindView(R.id.nextButton) Button mnextButton;
     @BindView(R.id.titlepage) TextView mtitlepage;
     @BindView(R.id.subtitlepage) TextView msubtitlepage;
     @BindView(R.id.intropage) TextView mintropage;
     @BindView(R.id.subintropage) TextView msubintropage;
     @BindView(R.id.divpage) View mdivpage;
-    @BindView(R.id.beginnerRadioButton) RadioButton mbeginnerExercise;
-    @BindView(R.id.intermediateRadioButton) RadioButton mintermediateExercise;
-    @BindView(R.id.advancedRadioButton) RadioButton madvancedExercise;
+    @BindView(R.id.absButton) RadioButton mbeginnerExercise;
+    @BindView(R.id.armsButton) RadioButton mintermediateExercise;
+    @BindView(R.id.backButton) RadioButton madvancedExercise;
 
 
     @Override
