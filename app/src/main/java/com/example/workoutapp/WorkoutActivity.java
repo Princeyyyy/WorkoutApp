e3fed44af34fd9998b40ceb797dd171a2b5ff94a
Princeyyyy <princemumo80@gmail.com>
2020-11-16 21:54:56 +0300

    binded radio buttons

--- app/src/main/java/com/example/workoutapp/WorkoutActivity.java
+++ app/src/main/java/com/example/workoutapp/WorkoutActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -16,6 +17,10 @@ public class WorkoutActivity extends AppCompatActivity {
     @BindView(R.id.intropage) TextView mintropage;
     @BindView(R.id.subintropage) TextView msubintropage;
     @BindView(R.id.divpage) View mdivpage;
+    @BindView(R.id.beginnerRadioButton) RadioButton mbeginnerExercise;
+    @BindView(R.id.intermediateRadioButton) RadioButton mintermediateExercise;
+    @BindView(R.id.advancedRadioButton) RadioButton madvancedExercise;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
