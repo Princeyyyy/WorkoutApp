986da03e5d2956de23e7afd00a07968f0071c07f
Princeyyyy <princemumo80@gmail.com>
2020-12-01 02:57:40 +0300

    intergrated firebase to app by pushing data

--- app/src/main/java/com/example/workoutapp/ui/SignUpActivity.java
+++ app/src/main/java/com/example/workoutapp/ui/SignUpActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.workoutapp.R;
+import com.example.workoutapp.UserHelperClass;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,12 +18,13 @@ import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
     Button signUp, toLogin;
+    TextInputLayout regName, regUsername, regEmail, regNumber, regPassword;
 //    @BindView(R.id.sign_up_btn) Button mSignUpButton;
-    @BindView(R.id.username) TextInputLayout mUserName;
-    @BindView(R.id.password) TextInputLayout mPassword;
-    @BindView(R.id.name) TextInputLayout mName;
-    @BindView(R.id.email) TextInputLayout mEmail;
-    @BindView(R.id.phoneNumber) TextInputLayout mPhoneNumber;
+//    @BindView(R.id.username) TextInputLayout mUserName;
+//    @BindView(R.id.password) TextInputLayout mPassword;
+//    @BindView(R.id.name) TextInputLayout mName;
+//    @BindView(R.id.email) TextInputLayout mEmail;
+//    @BindView(R.id.phoneNumber) TextInputLayout mPhoneNumber;
 
     FirebaseDatabase rootNode;
     DatabaseReference reference;
@@ -34,22 +36,37 @@ public class SignUpActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         signUp = findViewById(R.id.sign_up_btn);
-        toLogin = findViewById(R.id.to_login_btn);
+        regName = findViewById(R.id.name);
+        regUsername = findViewById(R.id.username);
+        regEmail = findViewById(R.id.email);
+        regNumber = findViewById(R.id.phoneNumber);
+        regPassword = findViewById(R.id.password);
 
-        toLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
+//        toLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
         signUp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 rootNode = FirebaseDatabase.getInstance();
                 reference = rootNode.getReference("users");
-                reference.setValue("First Data Storage");
+
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String number = regNumber.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass(name,username,email,number,password);
+
+                reference.child(number).setValue(helperClass);
             }
         });
         }
