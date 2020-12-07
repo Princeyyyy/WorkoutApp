cda67564c55e724e75a53585ab69aab5739ce01e
Princeyyyy <princemumo80@gmail.com>
2020-12-07 15:48:31 +0300

    added splash animation

--- app/src/main/java/com/example/workoutapp/ui/IntroActivity.java
+++ app/src/main/java/com/example/workoutapp/ui/IntroActivity.java
@@ -2,25 +2,50 @@ package com.example.workoutapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.workoutapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class IntroActivity extends AppCompatActivity {
+
+    private static final int SPLASH = 3300;
+
+    @BindView(R.id.image) ImageView mImage;
+    @BindView(R.id.text) TextView mText;
     Animation topAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_intro);
+        ButterKnife.bind(this);
 
         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
         bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
 
-//        mAppNameTextView.setAnimation(bottomAnim);
+        mImage.setAnimation(topAnim);
+        mText.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
     }
 }
