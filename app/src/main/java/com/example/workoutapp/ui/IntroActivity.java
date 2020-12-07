f382b005cd8410ed950d261c7367214146b6032b
Princeyyyy <princemumo80@gmail.com>
2020-12-07 15:36:34 +0300

    added new Activity

---
+++ app/src/main/java/com/example/workoutapp/ui/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.workoutapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.workoutapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+    Animation topAnim, bottomAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_intro);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+//        mAppNameTextView.setAnimation(bottomAnim);
+    }
+}
--- app/src/main/java/com/example/workoutapp/ui/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/ui/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,12 +19,19 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.startWorkoutButton) Button mStartWorkoutButton;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+//    Animation topAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+//        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+//        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+//
+//        mAppNameTextView.setAnimation(bottomAnim);
 
         mStartWorkoutButton.setOnClickListener(this);
     }
