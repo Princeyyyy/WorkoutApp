2cb39e926a6e70026f17144acc588cd509b1bc66
Princeyyyy <princemumo80@gmail.com>
2021-01-06 15:56:34 +0300

    updated login and sign up activity

--- app/src/main/java/com/example/workoutapp/ui/LoginActivity.java
+++ app/src/main/java/com/example/workoutapp/ui/LoginActivity.java
@@ -1,145 +1,16 @@
 package com.example.workoutapp.ui;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 import com.example.workoutapp.R;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    Button signUpScreen, login_btn;
-    TextView logoText, sloganText;
-    TextInputLayout username, password;
-
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_in);
-
-        signUpScreen = findViewById(R.id.signup_screen);
-        login_btn = findViewById(R.id.login_btn);
-        logoText = findViewById(R.id.logo_name);
-        sloganText = findViewById(R.id.slogan_name);
-        username = findViewById(R.id.login_username);
-        password = findViewById(R.id.login_password);
-
-        signUpScreen.setOnClickListener(this);
-        login_btn.setOnClickListener(this);
-
-    }
-    @Override
-    public void onClick(View v) {
-        if (v == signUpScreen){
-            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-            startActivity(intent);
-        }
-        if (v == login_btn){
-            Toast.makeText(LoginActivity.this, "Login Successful" , Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(LoginActivity.this, WorkoutActivity.class);
-            startActivity(intent);
-        }
-    }
-
-
-
-
-    private boolean validateUserName() {
-      String val = username.getEditText().getText().toString();
-
-      if (val.isEmpty()){
-          username.setError("Field cannot be Empty");
-          return false;
-      }else{
-          username.setError(null);
-          username.setErrorEnabled(false);
-          return true;
-      }
-    }
-
-    private boolean validatePassword(){
-        String val = password.getEditText().getText().toString();
-
-        if (val.isEmpty()){
-            password.setError("Field cannot be Empty");
-            return false;
-        }else{
-            password.setError(null);
-            password.setErrorEnabled(false);
-            return true;
-        }
-    }
-
-    private void loginUser(View view){
-        //Validate Login Info
-        if (!validateUserName() | !validatePassword()) {
-            return;
-        }else {
-            isUser();
-        }
-    }
-
-    private void isUser() {
-
-        String userEnteredUsername = username.getEditText().getText().toString().trim();
-        String userEnteredPassword = password.getEditText().getText().toString().trim();
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
-
-        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUsername);
-
-        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                if (dataSnapshot.exists()){
-
-                    username.setError(null);
-                    username.setErrorEnabled(false);
-
-                    String passwordFromDB = dataSnapshot.child(userEnteredUsername).child("password").getValue(String.class);
-
-                    if (passwordFromDB.equals(userEnteredPassword)){
-
-                        username.setError(null);
-                        username.setErrorEnabled(false);
-
-                        Intent intent =  new Intent(getApplicationContext(), WorkoutActivity.class);
-                        startActivity(intent);
-
-                    }else{
-                        password.setError("Wrong Password");
-                        password.requestFocus();
-
-                    }
-                }else{
-                    username.setError("No Such User Exists");
-                    username.requestFocus();
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/com/example/workoutapp/ui/SignUpActivity.java
+++ app/src/main/java/com/example/workoutapp/ui/SignUpActivity.java
@@ -2,77 +2,15 @@ package com.example.workoutapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.workoutapp.R;
-import com.example.workoutapp.UserHelperClass;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
-    Button signUp, toLogin;
-    TextInputLayout regName, regUsername, regEmail, regNumber, regPassword;
-
-    FirebaseDatabase rootNode;
-    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        ButterKnife.bind(this);
-
-        signUp = findViewById(R.id.sign_up_btn);
-        regName = findViewById(R.id.name);
-        regUsername = findViewById(R.id.username);
-        regEmail = findViewById(R.id.email);
-        regNumber = findViewById(R.id.phoneNumber);
-        regPassword = findViewById(R.id.password);
-        toLogin = findViewById(R.id.to_login_btn);
-
-
-
-        toLogin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                if(v == toLogin) {
-                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                }
-
-            }
-        });
-
-        signUp.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-                rootNode = FirebaseDatabase.getInstance();
-                reference = rootNode.getReference("users");
-
-                String name = regName.getEditText().getText().toString();
-                String username = regUsername.getEditText().getText().toString();
-                String email = regEmail.getEditText().getText().toString();
-                String number = regNumber.getEditText().getText().toString();
-                String password = regPassword.getEditText().getText().toString();
-
-                UserHelperClass helperClass = new UserHelperClass(name,username,email,number,password);
-
-                reference.child(number).setValue(helperClass);
-
-                Intent intent = new Intent(SignUpActivity.this, WorkoutActivity.class);
-                startActivity(intent);
-                Toast.makeText(SignUpActivity.this, "Your Account has Been Created" , Toast.LENGTH_SHORT).show();
-            }
-        });
-        }
+        setContentView(R.layout.activity_registration);
+    }
 }
