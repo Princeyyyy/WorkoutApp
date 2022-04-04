3eb3640684ed91839e116a8cd494cd994c4eac54
Princeyyyy <princemumo80@gmail.com>
2022-04-05 02:09:37 +0300

    fixing merge conflicts

--- app/src/main/java/com/example/workoutapp/constants/Constants.java
+++ app/src/main/java/com/example/workoutapp/constants/Constants.java
@@ -4,5 +4,5 @@ import com.example.workoutapp.BuildConfig;
 
 public class Constants {
     public static final String WGER_BASE_URL = "https://wger.de/api/v2/";
-    public static final String WGER_API_KEY = BuildConfig.WGER_API_KEY;
+    public static final String WGER_API_KEY = "Bearer b3ac664bb1638235811f06b50d7db3c2384992c6";
 }
--- app/src/main/java/com/example/workoutapp/network/WgerClient.java
+++ app/src/main/java/com/example/workoutapp/network/WgerClient.java
@@ -1,18 +1,12 @@
 package com.example.workoutapp.network;
 
-import com.example.workoutapp.network.WgerApi;
-
-import java.io.IOException;
-
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 import static com.example.workoutapp.constants.Constants.WGER_API_KEY;
 import static com.example.workoutapp.constants.Constants.WGER_BASE_URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WgerClient {
 
@@ -22,14 +16,11 @@ public class WgerClient {
 
         if (retrofit == null) {
             OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                    .addInterceptor(new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Request newRequest  = chain.request().newBuilder()
-                                    .addHeader("Authorization", WGER_API_KEY)
-                                    .build();
-                            return chain.proceed(newRequest);
-                        }
+                    .addInterceptor(chain -> {
+                        Request newRequest = chain.request().newBuilder()
+                                .addHeader("Authorization", WGER_API_KEY)
+                                .build();
+                        return chain.proceed(newRequest);
                     })
                     .build();
 
